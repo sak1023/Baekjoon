cf92261a569eb0c429f4ca5c6aba704154ccfd1d
HyeonSeok Lee <106586240+sak1023@users.noreply.github.com>
2023-06-12 15:32:11 +0900

    [Silver IV] Title: solved.ac, Time: 352 ms, Memory: 37772 KB -BaekjoonHub

--- 백준/Silver/18110. solved．ac/solved．ac.java
+++ 백준/Silver/18110. solved．ac/solved．ac.java
@@ -12,7 +12,7 @@ public class Main {
         
         int n = Integer.parseInt(br.readLine());
         int[] nums = new int[n];
-        int trim = Get_Round(n * 0.15);
+        int trim = (int) Math.round(n * 0.15);
         double sum = 0;
 
         for (int i = 0; i < n; i++) {
@@ -25,12 +25,8 @@ public class Main {
             sum += nums[i];
         }
 
-        bw.write(Integer.toString(Get_Round(sum / (n - trim * 2))));
+        bw.write(Integer.toString((int) Math.round(sum / (n - trim * 2))));
         bw.flush();
         bw.close();
-    }
-
-    private static int Get_Round(double d) {
-        return d - (int) d >= 0.5 ? (int) d + 1 : (int) d;
     }
 }
