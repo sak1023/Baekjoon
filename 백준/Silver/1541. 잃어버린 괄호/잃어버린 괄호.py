4c82cd7b99a6828d9b6aa52f99b32acb5dba2a15
HyeonSeok Lee <106586240+sak1023@users.noreply.github.com>
2023-06-30 15:16:34 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 84 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.py
@@ -0,0 +1,14 @@
+import sys
+
+array = sys.stdin.readline().split("-")
+
+result = 0
+
+for i in array[0].split("+"):
+    result += int(i)
+    
+for i in array[1:]:
+    for j in i.split("+"):
+        result -= int(j)
+            
+print(result)
