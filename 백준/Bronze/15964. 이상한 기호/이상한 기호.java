f4b2a2e8aa0d9b7ca1f5fc05a57b51a67c29f488
HyeonSeok Lee <106586240+sak1023@users.noreply.github.com>
2024-01-19 02:20:37 +0900

    [Bronze V] Title: 이상한 기호, Time: 120 ms, Memory: 14212 KB, Score: 100 point -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,21 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main (String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        long A = Integer.parseInt(st.nextToken());
+        long B = Integer.parseInt(st.nextToken());
+
+        System.out.print(At_Sign(A, B));
+    }
+
+    public static long At_Sign(long A, long B) {
+        return (A + B) * (A - B);
+    }
+}
